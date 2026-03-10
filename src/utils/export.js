cff5a6dca935ab78ca2a5232565a8c796f706d4a
contamfp81-por <conta.mfp81@gmail.com>
2026-03-10 10:20:41 -0300

    Delete src/utils/export.js

--- src/utils/export.js
+++
@@ -1,8 +0,0 @@
-import * as XLSX from 'xlsx';
-
-export const exportToExcel = (data, filename = 'gastos_contabeis.xlsx') => {
-    const worksheet = XLSX.utils.json_to_sheet(data);
-    const workbook = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(workbook, worksheet, 'Relatório');
-    XLSX.writeFile(workbook, filename);
-};
